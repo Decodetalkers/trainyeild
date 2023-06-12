62cf531bc6dba831058015883666a9cd49532ed7
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-06-12 11:03:22 +0800

    chore: style format

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,25 @@
 
 mod elements;
 
+use elements::CliElement;
+
 fn main() {
-    let test = elements::CliElement::print_column(|| {
-        let unit = elements::CliElement::print_singal(&["sss"]);
-        yield elements::CliElement::print_row(move || {
+    CliElement::print_column(|| {
+        let unit = CliElement::print_singal(&["sss"]);
+        yield CliElement::print_row(move || {
             let unita = unit.clone();
-            yield elements::CliElement::print_singal(&["beta"]);
-            yield elements::CliElement::print_singal(&["  "]);
+            yield CliElement::print_singal(&["beta"]);
+            yield CliElement::print_singal(&["  "]);
             yield unita.clone();
 
-            yield elements::CliElement::print_column(|| {
-                yield elements::CliElement::print_emptyblock();
-                yield elements::CliElement::print_singal(&["gammer", "gammer", "beta"]);
+            yield CliElement::print_column(|| {
+                yield CliElement::print_emptyblock();
+                yield CliElement::print_singal(&["gammer", "gammer", "beta"]);
             });
             yield unit
         });
-        yield elements::CliElement::print_singal(&["beta"]);
-        yield elements::CliElement::print_singal(&["alpha"])
-    });
-    test.draw();
+        yield CliElement::print_singal(&["beta"]);
+        yield CliElement::print_singal(&["alpha"])
+    })
+    .draw();
 }
