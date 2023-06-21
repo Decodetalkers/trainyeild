79fc626bfd065b75cece00275d0eb8852684e135
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-06-21 10:47:32 +0800

    chore: bring back nightly

--- cliprint/examples/simple.rs
+++ cliprint/examples/simple.rs
@@ -1,3 +1,4 @@
+#![feature(generators, generator_trait)]
 use cliprint::elements;
 use cliprint::layout;
 use cliprint::layout::RowSettings;
@@ -8,6 +9,7 @@ use nu_ansi_term::Color::Cyan;
 
 const ARCHLINUX: &str = include_str!("../assert/archlinux.txt");
 
+#[cfg(not(feature = "nightly"))]
 fn main() {
     CliElement::print_row(
         vec![
@@ -43,5 +45,38 @@ fn main() {
         .into_iter(),
         Some(RowSettings { spacing: 1 }),
     )
+    .draw();
+}
+
+#[cfg(feature = "nightly")]
+fn main() {
+    CliElement::print_row(|| {
+        yield CliElement::print_singal_from_str_with_color(ARCHLINUX, Alignment::Left, Cyan, true);
+        yield CliElement::print_column(|| {
+            yield CliElement::print_singal(
+                &[&Cyan.bold().paint("name: marine").to_string()],
+                Alignment::Left,
+            );
+            yield CliElement::print_singal(&["------------"], Alignment::Left);
+            yield CliElement::print_singal(&["OS: ArchLinux"], Alignment::Left);
+            yield CliElement::print_singal(&["Host: Yoga 15s"], Alignment::Left);
+            yield CliElement::print_singal(&["Kernel: 6.3.0-arch1-1"], Alignment::Left);
+            yield CliElement::print_singal(&["Wm: sway"], Alignment::Left);
+            yield CliElement::print_singal(&["terminal: Wezterm"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+            yield CliElement::print_singal(&["Theme: Breeze"], Alignment::Left);
+        });
+        Some(RowSettings { spacing: 1 })
+    })
     .draw();
 }
